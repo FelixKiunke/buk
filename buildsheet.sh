c679eb9eee8385547555a5b5a0f3112f4a50d8f3
Felix Kiunke <dev@fkiunke.de>
2016-11-09 15:05:54 +0100

    Add build script

---
+++ buildsheet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sheet=$(dirname $1)
+sheetnum=$(echo "$sheet" | sed 's/^u0//' | sed 's/^u//')
+
+exs=$(ls "$sheet" | grep 'u[0-9][0-9]a[0-9]' | sed 's/^.*a//' | sed 's/\.tex$//')
+
+cp template.tex $sheet/$sheet.tex
+
+sed -i "s/SHEETNUM/$sheetnum/" $sheet/$sheet.tex
+
+echo -n "" > $sheet/tmp.tex
+for n in $exs; do
+	echo '\aufgabe{'"$sheetnum.$n"'}' >> $sheet/tmp.tex
+	echo '\input{'"${sheet}a$n.tex"'}' >> $sheet/tmp.tex
+done
+
+sed -i "/% EXERCISES/r $sheet/tmp.tex" $sheet/$sheet.tex
+
+rm "$sheet/tmp.tex"
